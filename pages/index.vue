af8014b8da1c3b2b09f345de0e8e13333892cf07
fukuma_ryutaro <fukuma_ryutaro@weddingpark.co.jp>
2019-12-17 23:48:55 +0900

    topページの改修

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,26 @@
 <template>
   <div>
-    <Play :src="src" :results="results" :random="random"/>
+    <v-btn to="/search">アーティストを検索する</v-btn>
+    <v-btn>プレイリスト一覧</v-btn>
   </div>
 </template>
 
 <script>
-import Play from '~/components/Play.vue'
-import axios from 'axios'
+// import Play from '~/components/Play.vue'
+// import axios from 'axios'
 
 export default {
-    async asyncData ({}) {
-        const random = Math.floor( Math.random() * 10 ) + 1
-        const { data } = await axios.get(`http://itunes.apple.com/search?term=ohara yuiko&country=JP&lang=ja_jp&media=music&entity=song&attribute=artistTerm&limit=30`)
-          return {
-            results: data.results,
-            src: data.results[random].previewUrl,
-            random: random
-          }
-    },
-    components: {
-      Play
-    },
+    // async asyncData ({}) {
+    //     const random = Math.floor( Math.random() * 10 ) + 1
+    //     const { data } = await axios.get(`http://itunes.apple.com/search?term=ohara yuiko&country=JP&lang=ja_jp&media=music&entity=song&attribute=artistTerm&limit=30`)
+    //       return {
+    //         results: data.results,
+    //         src: data.results[random].previewUrl,
+    //         random: random
+    //       }
+    // },
+    // components: {
+    //   Play
+    // },
 }
 </script>
