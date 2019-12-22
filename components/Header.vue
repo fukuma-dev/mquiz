ba0d6c4019c0c70ab1f919333d9cb6992d20ae7c
fukuma_ryutaro <ricken0203@gmail.com>
2019-12-22 23:21:44 +0900

    ヘッダー追加

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-items>
+      <v-btn to="/" class="logo" active-class flat>
+        <h1>mquiz</h1>
+      </v-btn>
+    </v-toolbar-items>
+    <v-spacer></v-spacer>
+    <v-toolbar-items v-for="item in items" :key="item.text">
+      <v-btn :to="item.to" active-class flat>{{ item.text }}</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    },
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,33 @@
 <template>
   <v-app dark>
-    <v-content>
-      <v-container>
-        <h1>mQuiz</h1>
-        <nuxt />
-      </v-container>
-    </v-content>
+    <custom-header :items="items"/>
+    <nuxt />
   </v-app>
 </template>
 
 <script>
-
+export default {
+  components: {
+    CustomHeader: () => import('~/components/Header.vue'),
+  },
+  data() {
+    return {
+      isDrawerOpen: false,
+      items: [
+        {
+          text: 'Top',
+          to: '/'
+        },
+        {
+          text: 'Search',
+          to: '/search'
+        },
+        {
+          text: 'Playlist',
+          to: '/playlist'
+        },
+      ]
+    }
+  },
+}
 </script>
