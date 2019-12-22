362b7bf56234e65e3195d791a45d250da5513cd4
fukuma_ryutaro <ricken0203@gmail.com>
2019-12-22 23:23:31 +0900

    Searchコンポーネント追加

---
+++ components/Search.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container>
+    <v-layout>
+        <v-text-field v-model="term" placeholder="スピッツ" solo></v-text-field>
+        <v-btn v-on:click="fetchResults(term)" large>検索</v-btn>
+    </v-layout>
+    <v-card v-for="n in results" :key="n.artistId" class="my-3 artist-list" :to="`/artists/${n.artistId}`" hover="true">
+      <v-card-title>{{ n.artistName }}</v-card-title>
+    </v-card>
+    <p v-if="count === 0">"{{ term }}"に該当するアーティストが見つかりませんでした</p>
+  </v-container>
+</template>
+
+<style>
+.artist-list {
+    list-style: none;
+    border-left: solid 10px cadetblue;
+    padding: 10px 0 10px 15px;
+}
+.artist-name {
+    text-decoration: none;
+    color: #fff;
+    font-size: 1.5rem;
+}
+.artist-name:hover {
+    opacity: 0.3;
+}
+</style>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+      return {
+        term: '',
+        results: '',
+        count: false,
+      }
+    },
+    methods: {
+      fetchResults(term) {
+        axios.get(`http://itunes.apple.com/search?term=${term}&media=music&entity=musicArtist&attribute=artistTerm&country=JP&lang=ja_jp`)
+          .then(response => {
+            this.results = response.data.results
+            this.count = response.data.resultCount
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+      }
+    }
+}
+</script>
