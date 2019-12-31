4abf8d73f5d1d8f289990da4c927181d1fb7a0af
fukuma_ryutaro <ricken0203@gmail.com>
2020-01-01 01:59:51 +0900

    feat: 検索とプレイヤーの見た目を調整

--- frontend/components/Player.vue
+++ frontend/components/Player.vue
@@ -1,24 +1,42 @@
 <template>
   <div class="audio">
     <v-card>
+      <p v-show="res" class="message">{{ res }}</p>
       <v-card-text v-if="trackName">{{ trackName + ' / ' + artistName }}</v-card-text>
-      <vuetify-audio :file="src" ref="player" :canPlay="play()"></vuetify-audio>
+      <vuetify-audio :file="src" ref="player" :canPlay="play()" class="player"></vuetify-audio>
+      <v-btn @click="submit(trackId, trackName, previewUrl, artistName, artistId)" flat block large>この曲を追加する</v-btn>
     </v-card>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import VuetifyAudio from 'vuetify-audio'
 
 export default {
   props: {
+    res: '',
+    trackId: '',
     trackName: '',
+    artistId: '',
     artistName: '',
     src: '',
   },
   methods: {
+    submit(trackId, trackName, previewUrl, artistName, artistId) {
+      axios.post('http://localhost:3000/tracks', {
+        trackId: trackId,
+        trackName: trackName,
+        previewUrl: previewUrl,
+        artistName: artistName,
+        artistId: artistId,
+      })
+      .then(response => {
+        this.res = response.data
+      })
+    },
     play() {
-      console.log(this.$refs.player)
+      console.log('test')
     }
   },
   components: {
@@ -34,5 +52,18 @@ export default {
   bottom: 0;
   left: 0;
   margin: 15px;
+
+}
+.message {
+  position: absolute;
+  top: -60px;
+  left: 0;
+  z-index: 1;
+  padding: 5px;
+  color: #fff;
+  background-color: grey;
+}
+.player {
+  box-shadow: none;
 }
 </style>
--- frontend/components/Search.vue
+++ frontend/components/Search.vue
@@ -1,21 +1,33 @@
 <template>
-  <v-container>
+  <v-container class="pb-0 container">
     <v-layout>
-        <v-text-field v-model="term" v-on:keyup.enter="fetchResults(term)" @change="fetchResults(term)" placeholder="スピッツ" solo></v-text-field>
-        <v-btn @click="fetchResults(term)" large>検索</v-btn>
+      <v-text-field v-model="term" v-on:keyup.enter="fetchResults(term)" @change="fetchResults(term)" placeholder="スピッツ" solo></v-text-field>
+      <v-btn @click="fetchResults(term)">検索</v-btn>
     </v-layout>
-    <v-card v-for="n in results" :key="n.artistId" class="my-3 artist-list" :to="`/artists/${n.artistId}`" hover="true">
-      <v-card-title>{{ n.artistName }}</v-card-title>
-    </v-card>
+    <div v-if="count > 0" class="results">
+      <li v-for="n in results" :key="n.artistId" class="results__list">
+        <nuxt-link :to="`/artists/${n.artistId}`" class="results__list_link">{{ n.artistName }}</nuxt-link>
+      </li>
+    </div>
     <p v-if="count === 0">"{{ term }}"に該当するアーティストが見つかりませんでした</p>
   </v-container>
 </template>
 
 <style>
-.artist-list {
-    list-style: none;
-    border-left: solid 10px cadetblue;
-    padding: 10px 0 10px 15px;
+.container {
+  position: relative;
+}
+.results {
+  position: absolute;
+  padding: 10px;
+  width: 80%;
+  background-color: white;
+}
+.results__list {
+  list-style: none;
+}
+.results__list a {
+  text-decoration: none;
 }
 </style>
 
@@ -32,7 +44,7 @@ export default {
     },
     methods: {
       fetchResults(term) {
-        axios.get(`http://itunes.apple.com/search?term=${term}&media=music&entity=musicArtist&attribute=artistTerm&country=JP&lang=ja_jp`)
+        axios.get(`http://itunes.apple.com/search?lang=ja_jp&country=JP&term=${term}&media=music&entity=musicArtist&attribute=artistTerm&attribute=artistTerm&limit=20`)
           .then(response => {
             this.results = response.data.results
             this.count = response.data.resultCount
