98d4ab381f299c94b438b6b7478584e5cc9d7fd1
fukuma_ryutaro <ricken0203@gmail.com>
2020-01-01 00:25:47 +0900

    feat: Playerコンポーネント追加

---
+++ frontend/components/Player.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="audio">
+    <v-card>
+      <v-card-text v-if="trackName">{{ trackName + ' / ' + artistName }}</v-card-text>
+      <vuetify-audio :file="src" ref="player" :canPlay="play()"></vuetify-audio>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import VuetifyAudio from 'vuetify-audio'
+
+export default {
+  props: {
+    trackName: '',
+    artistName: '',
+    src: '',
+  },
+  methods: {
+    play() {
+      console.log(this.$refs.player)
+    }
+  },
+  components: {
+    'vuetify-audio': VuetifyAudio
+  }
+}
+</script>
+
+<style>
+.audio {
+  width: 30%;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  margin: 15px;
+}
+</style>
--- frontend/pages/artists/_id.vue
+++ frontend/pages/artists/_id.vue
@@ -1,63 +1,84 @@
 <template>
     <v-container>
+      <p v-if="res" class="message">{{ res }}</p>
       <h2>{{ results[1].artistName }}</h2>
       <p>{{ count-1 }}件</p>
-      <p>{{ res }}</p>
       <div v-for="n in results" :key="n.trackId">
         <v-card v-if="n.trackName" class="pa-2 my-2" outlined tile>
-          <v-avatar>
-            <img :src="n.artworkUrl100">
-          </v-avatar>
-          <span class="ml-2">{{ n.trackName }}</span>
-          <audio :src="n.previewUrl" :ref="'audio'+n.trackId"></audio>
-          <v-btn @click="start('audio' + n.trackId)">start</v-btn>
-          <v-btn @click="end('audio' + n.trackId)">end</v-btn>
-          <v-btn flat @click="submit(n.trackId, n.trackName, n.previewUrl)">この曲をプレイリストに追加</v-btn>
+          <v-layout row wrap>
+            <v-flex sm9 xs12 pa-0>
+              <v-avatar size="60" tile>
+                <img :src="n.artworkUrl100">
+              </v-avatar>
+              <span class="ml-2">{{ n.trackName }}</span>
+            </v-flex>
+            <v-flex sm3 xs12 pa-0>
+              <v-btn @click="set(n.artistName, n.trackName, n.previewUrl)" class="ml-3" fab small color="white">▷</v-btn>
+              <v-btn @click="submit(n.trackId, n.trackName, n.previewUrl, n.artistName, n.artistId)" class="ml-1" fab small color="white">＋</v-btn>
+            </v-flex>
+          </v-layout>
         </v-card>
       </div>
+      <Player :trackName="trackName" :artistName="artistName" :src="src"></Player>
     </v-container>
 </template>
 
 <script>
 import axios from 'axios'
-import Search from '~/components/Search.vue'
+import Player from '~/components/Player.vue'
 
 export default {
-    data() {
-      return {
-        res: '',
-      }
-    },
-    methods: {
-      start(ref) {
-        this.$refs[ref][0].play()
-      },
-      end(ref) {
-          console.log(this.$refs[ref][0])
-          this.$refs[ref][0].pause()
-          this.$refs[ref][0].currentTime = 0;
-      },
-      submit(trackId, trackName, previewUrl) {
-          axios.post('http://localhost:3000/tracks', {
-            trackId: trackId,
-            trackName: trackName,
-            previewUrl: previewUrl,
-          })
-          .then(response => {
-            this.res = response.data
-          })
-      },
-    },
-    async asyncData ({params}) {
-        const { data } = await axios.get(`http://itunes.apple.com/lookup?id=${params.id}&country=JP&lang=ja_jp&entity=song&sort=recent&limit=200`)
-
-        return {
-          results: data.results,
-          count: data.resultCount
-        }
-    },
-    components: {
-      Search
+  data() {
+    return {
+      res: '',
+      src: '',
+      trackName: '',
+      artistName: '',
+      canPlay: () => {}
     }
+  },
+  methods: {
+    set(artistName, trackName, previewUrl) {
+      this.src = previewUrl
+      this.trackName = trackName
+      this.artistName = artistName
+    },
+    submit(trackId, trackName, previewUrl, artistName, artistId) {
+      axios.post('http://localhost:3000/tracks', {
+        trackId: trackId,
+        trackName: trackName,
+        previewUrl: previewUrl,
+        artistName: artistName,
+        artistId: artistId,
+      })
+      .then(response => {
+        this.res = response.data
+      })
+    },
+  },
+  async asyncData ({params}) {
+      const { data } = await axios.get(`http://itunes.apple.com/lookup?id=${params.id}&country=JP&lang=ja_jp&entity=song&sort=recent&limit=200`)
+
+      return {
+        results: data.results,
+        count: data.resultCount
+      }
+  },
+  components: {
+    Player
+  }
 }
 </script>
+
+<style>
+.message {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  margin: 15px;
+  padding: 5px;
+  background-color: blanchedalmond;
+  opacity: 0.9;
+}
+</style>
