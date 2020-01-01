a9bd118927e0a368e8f5e179da24f16c70af4c42
fukuma_ryutaro <ricken0203@gmail.com>
2020-01-02 00:05:24 +0900

    feat: vuexでaudioの状態を管理する

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,39 +1,52 @@
 <template>
   <v-app>
     <custom-header></custom-header>
-        <v-container fluid>
-          <v-row>
-            <v-col cols="4">
-              <div class="side">
-                <h3>ユーザー情報</h3>
-                <p>ユーザー名が入ります</p>
-                <v-list>
-                  <v-list-item to="/">検索</v-list-item>
-                  <v-list-item to="/playlist">プレイリスト</v-list-item>
-                </v-list>
-              </div>
-            </v-col>
-            <v-col cols="8">
-              <nuxt />
-            </v-col>
-          </v-row>
-        </v-container>
+      <v-content>
+      <v-container>
+        <v-row>
+          <v-col cols="4">
+            <div>
+              <h3>ユーザー情報</h3>
+              <p>ユーザー名が入ります</p>
+              <v-list>
+                <v-list-item to="/">検索</v-list-item>
+                <v-list-item to="/playlist">プレイリスト</v-list-item>
+              </v-list>
+              <Player
+                :trackId="trackId"
+                :trackName="trackName"
+                :artistId="artistId"
+                :artistName="artistName"
+                :src="src"
+                class="player">
+              </Player>
+            </div>
+          </v-col>
+          <v-col cols="8">
+            <nuxt />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
 <script>
+import Player from '~/components/Player.vue'
+
 export default {
+  data() {
+    return {
+      trackId: '1234',
+      trackName: 'asdfa',
+      artistId: '1234',
+      artistName: 'asfas',
+      src: 'asdf',
+    }
+  },
   components: {
     CustomHeader: () => import('~/components/Header.vue'),
+    Player
   }
 }
 </script>
-
-<style>
-.side {
-  /* position: fixed;
-  width: 25%;
-  border: solid 1px;
-  padding: 10px; */
-}
-</style>
