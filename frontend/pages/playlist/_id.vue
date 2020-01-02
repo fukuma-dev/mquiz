d243ab80cc6a37e5b6e39372ad73bda709f65685
fukuma_ryutaro <ricken0203@gmail.com>
2020-01-02 21:39:02 +0900

    feat: プレイリストを簡単に実装

--- frontend/pages/playlist/_id.vue
+++ frontend/pages/playlist/_id.vue
@@ -1,29 +1,43 @@
 <template>
   <v-container>
     <nuxt-link to="/playlist">topへ戻る</nuxt-link>
-    <h2>プレイリスト名</h2>
-    <h3>第{{ n + 1 }}問</h3>
-    <div>
-      <audio :src="results[n].preview_url" autoplay></audio>
-      <div v-if="!answer">
-        <ul>
-          <li>選択肢1</li>
-          <li>選択肢2</li>
-          <li>選択肢3</li>
-          <li>選択肢4</li>
-        </ul>
-        <v-btn @click="switchPage">答えを見る</v-btn>
+    <div v-if="!isStart">
+      <v-btn @click="quizStart" block flat height="100px" class="start-btn">開始しますか？</v-btn>
+    </div>
+    <div v-if="isStart">
+      <h2>プレイリスト名</h2>
+      <h3>第{{ n + 1 }}問</h3>
+      <div>
+        <audio :src="results[n].preview_url" controls autoplay loop></audio>
+        <div v-if="!answer">
+          <v-row class="row my-3">
+            <v-col cols="6">
+              <p class="display-1">A</p><p class="display-3">その1の答えだよ</p>
+            </v-col>
+            <v-col cols="6">
+              <p class="display-1">B</p><p class="display-3">その1の答えだよ</p>
+            </v-col>
+          </v-row>
+          <v-row class="row my-3">
+            <v-col cols="6">
+              <p class="display-1">C</p><p class="display-3">その1の答えだよ</p>
+            </v-col>
+            <v-col cols="6">
+              <p class="display-1">D</p><p class="display-3">その1の答えだよ</p>
+            </v-col>
+          </v-row>
+          <v-btn @click="switchPage" block large flat height="50px">答えを見る</v-btn>
+        </div>
+      </div>
+      <div v-if="answer">
+       <h4>答え</h4>
+        <div class="text-center my-4">
+          <p class="display-4">{{ results[n].track_name }}<span class="display-3">/ {{ results[n].artist_name }}</span></p>
+          <v-btn v-if="trackExists(results, n)" @click="next" block large flat height="50px">次の問題へ</v-btn>
+          <v-btn v-else to="/" block large flat height="50px">終了</v-btn>
+        </div>
       </div>
     </div>
-    <div v-if="answer">
-     <h4>答え</h4>
-      <p>{{ results[n].track_name }} / {{ results[n].artist_name }}</p>
-      <v-btn @click="next">次</v-btn>
-    </div>
-    <!-- <div v-for="n in results" :key="n.trackId"> -->
-      <!-- <p>{{ n.track_name }}</p> -->
-      <!-- <audio :src="n.preview_url" controls preload="auto"></audio> -->
-    <!-- </div> -->
   </v-container>
 </template>
 
@@ -34,16 +48,23 @@ export default {
     data() {
       return {
         n: 0,
-        answer: false
+        answer: false,
+        isStart: false
       }
     },
     methods: {
+      quizStart () {
+        this.isStart = true
+      },
       switchPage () {
         this.answer = true
       },
       next () {
         this.n = this.n + 1
         this.answer = false
+      },
+      trackExists(results, n) {
+        return results[n+1] !== undefined
       }
     },
     async asyncData ({params}) {
@@ -51,10 +72,18 @@ export default {
         return {
           results: data,
         }
+    },
+    components: {
+      CustomAudio: () => import("~/components/Audio.vue")
     }
 }
 </script>
 
 <style scoped>
-
+.row {
+  height: 200px;
+}
+.start-btn {
+  margin: 200px 50px;
+}
 </style>
