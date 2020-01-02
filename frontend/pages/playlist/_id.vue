cb571cf15d7ad69476909ff1260f450fdd5e0791
fukuma_ryutaro <ricken0203@gmail.com>
2020-01-02 23:20:43 +0900

    feat: flat->textに書き換え

--- frontend/pages/playlist/_id.vue
+++ frontend/pages/playlist/_id.vue
@@ -2,7 +2,7 @@
   <v-container>
     <nuxt-link to="/playlist">topへ戻る</nuxt-link>
     <div v-if="!isStart">
-      <v-btn @click="quizStart" block flat height="100px" class="start-btn">開始しますか？</v-btn>
+      <v-btn @click="quizStart" block text outlined height="100px" class="start-btn">開始しますか？</v-btn>
     </div>
     <div v-if="isStart">
       <h2>プレイリスト名</h2>
@@ -26,15 +26,15 @@
               <p class="display-1">D</p><p class="display-3">その1の答えだよ</p>
             </v-col>
           </v-row>
-          <v-btn @click="switchPage" block large flat height="50px">答えを見る</v-btn>
+          <v-btn @click="switchPage" block large text outlined height="50px">答えを見る</v-btn>
         </div>
       </div>
       <div v-if="answer">
        <h4>答え</h4>
         <div class="text-center my-4">
           <p class="display-4">{{ results[n].track_name }}<span class="display-3">/ {{ results[n].artist_name }}</span></p>
-          <v-btn v-if="trackExists(results, n)" @click="next" block large flat height="50px">次の問題へ</v-btn>
-          <v-btn v-else to="/" block large flat height="50px">終了</v-btn>
+          <v-btn v-if="trackExists(results, n)" @click="next" block large text outlined height="50px">次の問題へ</v-btn>
+          <v-btn v-else to="/" block large text outlined height="50px">終了</v-btn>
         </div>
       </div>
     </div>
