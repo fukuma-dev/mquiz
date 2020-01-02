b0f809734da30602a74d6b530bb1cbbd27faebec
fukuma_ryutaro <ricken0203@gmail.com>
2020-01-02 21:37:54 +0900

    feat: サイドメニューをコンポーネントにした

---
+++ frontend/components/Menu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- <h3>ユーザー情報</h3>
+    <p>ユーザー名が入ります</p> -->
+    <v-list class="pa-0" flat>
+      <v-list-item to="/" tile>検索</v-list-item>
+      <v-list-item to="/playlist">プレイリスト</v-list-item>
+    </v-list>
+    <audio-player
+      :trackId="trackId"
+      :trackName="trackName"
+      :artistId="artistId"
+      :artistName="artistName"
+      :src="src"
+      class="player"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      trackId: "",
+      trackName: "",
+      artistId: "",
+      artistName: "",
+      src: ""
+    }
+  },
+  components: {
+    AudioPlayer: () => import("~/components/AudioPlayer.vue")
+  }
+}
+</script>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -4,25 +4,10 @@
     <v-content>
       <v-container>
         <v-row>
-          <v-col cols="4">
-            <div>
-              <h3>ユーザー情報</h3>
-              <p>ユーザー名が入ります</p>
-              <v-list>
-                <v-list-item to="/">検索</v-list-item>
-                <v-list-item to="/playlist">プレイリスト</v-list-item>
-              </v-list>
-              <audio-player
-                :trackId="trackId"
-                :trackName="trackName"
-                :artistId="artistId"
-                :artistName="artistName"
-                :src="src"
-                class="player"
-              />
-            </div>
+          <v-col cols="4" v-if="$route.name !== 'playlist-id'">
+            <side-menu />
           </v-col>
-          <v-col cols="8">
+          <v-col :cols="{ '8': $route.name !== 'playlist-id', '12': $route.name === 'playlist-id' }">
             <nuxt />
           </v-col>
         </v-row>
@@ -35,16 +20,16 @@
 export default {
   data() {
     return {
-      trackId: "1234",
-      trackName: "asdfa",
-      artistId: "1234",
-      artistName: "asfas",
-      src: "asdf"
-    };
+      trackId: "",
+      trackName: "",
+      artistId: "",
+      artistName: "",
+      src: ""
+    }
   },
   components: {
     CustomHeader: () => import("~/components/Header.vue"),
-    AudioPlayer: () => import("~/components/AudioPlayer.vue")
+    SideMenu: () => import("~/components/Menu.vue")
   }
 };
 </script>
